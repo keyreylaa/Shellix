20f61fefaa193f042406d6613ab9eb1dbc8aec72
keyreylaa <zyskyv@gmail.com>
2026-07-17 22:06:43 +0700

    fix: add missing JUnit test deps to core:resources build

--- core/resources/build.gradle.kts
+++ core/resources/build.gradle.kts
@@ -32,4 +32,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.junit)
+    androidTestImplementation(libs.androidx.test.espresso)
 }
